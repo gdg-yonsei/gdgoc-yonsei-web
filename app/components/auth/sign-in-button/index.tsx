643f8563e909c6327ba4f317f754fe18cc55e378
jhyunwoo <jhyunwoo0228@gmail.com>
2024-12-08 15:09:29 +0900

    useActionState를 useFormStatus로 변경

--- app/components/auth/sign-in-button/index.tsx
+++ app/components/auth/sign-in-button/index.tsx
@@ -1,33 +1,35 @@
 "use client";
 
-import { useActionState } from "react";
-import { TailSpin } from "react-loader-spinner";
 import { signInAction } from "@/app/components/auth/sign-in-button/actions";
+import { useFormStatus } from "react-dom";
+import { TailSpin } from "react-loader-spinner";
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="submit"
+      disabled={pending}
+      className={
+        "flex gap-2 items-center p-2 px-4 rounded-full bg-neutral-900 text-white transition-all"
+      }
+    >
+      <TailSpin
+        visible={pending}
+        color="#ffffff"
+        radius="1"
+        width={20}
+        height={20}
+      />
+      Sign In with Github
+    </button>
+  );
+}
 
 export default function SignInWithGithubButton() {
-  const [, submitAction, isPending] = useActionState(
-    () => signInAction("github"),
-    null,
-  );
-
   return (
-    <form action={submitAction}>
-      <button
-        type="submit"
-        disabled={isPending}
-        className={
-          "flex gap-2 items-center p-2 px-4 rounded-full bg-neutral-900 text-white transition-all"
-        }
-      >
-        <TailSpin
-          visible={isPending}
-          color="#ffffff"
-          radius="1"
-          width={20}
-          height={20}
-        />
-        Sign In with Github
-      </button>
+    <form action={() => signInAction("github")}>
+      <SubmitButton />
     </form>
   );
 }
--- app/components/auth/sign-out-button/index.tsx
+++ app/components/auth/sign-out-button/index.tsx
@@ -1,31 +1,35 @@
 "use client";
 
-import { useActionState } from "react";
 import { TailSpin } from "react-loader-spinner";
 import signOutAction from "@/app/components/auth/sign-out-button/actions";
+import { useFormStatus } from "react-dom";
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="submit"
+      disabled={pending}
+      className={
+        "flex gap-2 items-center bg-neutral-900 text-white p-2 px-4 rounded-full transition-all"
+      }
+    >
+      <TailSpin
+        visible={pending}
+        color="#ffffff"
+        radius="1"
+        width={20}
+        height={20}
+      />
+      Sign Out
+    </button>
+  );
+}
 
 export function SignOutButton() {
-  const [, submitAction, isPending] = useActionState(signOutAction, null);
-
   return (
-    <form action={submitAction}>
-      <button
-        type="submit"
-        disabled={isPending}
-        className={
-          "flex gap-2 items-center bg-neutral-900 text-white p-2 px-4 rounded-full transition-all"
-        }
-      >
-        <TailSpin
-          visible={isPending}
-          color="#ffffff"
-          radius="1"
-          width={20}
-          height={20}
-          wrapperStyle={{}}
-        />
-        Sign Out
-      </button>
+    <form action={signOutAction}>
+      <SubmitButton />
     </form>
   );
 }
