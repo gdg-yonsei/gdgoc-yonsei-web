46576f96a373dfe2418cbfa5889c5e5bd41775f0
jhyunwoo <jhyunwoo0228@gmail.com>
2025-01-02 15:08:17 +0900

    fix linked in update error

--- app/admin/members/[memberId]/edit/actions.ts
+++ app/admin/members/[memberId]/edit/actions.ts
@@ -22,7 +22,7 @@ export async function updateMemberAction(memberId: string, formData: FormData) {
   const email = formData.get('email') as string | null
   const githubId = formData.get('githubId') as string | null
   const instagramId = formData.get('instagramId') as string | null
-  const linkedInId = formData.get('linkedinId') as string | null
+  const linkedInId = formData.get('linkedInId') as string | null
   const major = formData.get('major') as string | null
   const studentId = formData.get('studentId') as string | null
   const telephone = formData.get('telephone') as string | null
