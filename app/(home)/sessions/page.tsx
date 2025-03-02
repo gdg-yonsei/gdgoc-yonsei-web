8086b68eeb08ebad22c7b3903d8a405359e501a9
jhyunwoo <jhyunwoo0228@gmail.com>
2025-03-02 15:57:27 +0900

    add session page

--- app/(home)/sessions/page.tsx
+++ app/(home)/sessions/page.tsx
@@ -1,7 +1,14 @@
-export default function SessionsPage() {
+import PageTitle from '@/app/components/page-title'
+import StageButtonGroup from '@/app/components/stage-button-group'
+import { getGenerations } from '@/lib/fetcher/get-generations'
+
+export default async function SessionsPage() {
+  const generationsData = await getGenerations()
+
   return (
     <div className={'w-full min-h-screen pt-16'}>
-      <div>Sessions Page</div>
+      <PageTitle>Sessions</PageTitle>
+      <StageButtonGroup generationsData={generationsData} />
     </div>
   )
 }
