f220ec8579f9000f6d2a86d8f155f76797e0ffcb
jhyunwoo <jhyunwoo0228@gmail.com>
2025-03-05 17:20:59 +0900

    add desktop layout on footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,32 +2,59 @@ import Link from 'next/link'
 import { EnvelopeIcon } from '@heroicons/react/24/outline'
 import LinkedIn from '@/app/components/svg/linked-in'
 import Instagram from '@/app/components/svg/instagram'
+import GDGoCYonseiLogo from '@/app/components/svg/gdgoc-yonsei-logo'
 
 export default function Footer() {
   return (
-    <div className={'w-full p-4 bg-neutral-400'}>
-      <div className={'text-white text-lg font-semibold'}>Contact Us</div>
-      <div className={'flex justify-around py-4 items-center'}>
-        <Link href={'mailto:gdsc.yonsei.univ@gmail.com'} target={'_blank'}>
-          <EnvelopeIcon className={'size-10 text-neutral-700'} />
-        </Link>
-        <Link
-          href={'https://www.linkedin.com/company/gdsc-yonsei/'}
-          target={'_blank'}
-        >
-          <LinkedIn className={'size-9'} />
-        </Link>
-        <Link
-          href={'https://www.instagram.com/gdg.yonseiuniv/'}
-          target={'_blank'}
-        >
-          <Instagram className={'size-10'} />
-        </Link>
+    <div className={'w-full p-4 bg-neutral-400 flex'}>
+      <div className={'w-full max-w-4xl mx-auto md:flex'}>
+        <div className={'flex flex-col gap-6 w-full'}>
+          <div className={'text-white text-lg font-semibold md:text-3xl'}>
+            Contact Us
+          </div>
+          <GDGoCYonseiLogo className={'not-md:hidden'} />
+        </div>
+        <div className={'md:flex md:flex-col md:items-start md:w-full'}>
+          <div
+            className={
+              'flex justify-around py-4 items-center md:flex-col md:gap-2 md:items-start'
+            }
+          >
+            <Link
+              href={'mailto:gdsc.yonsei.univ@gmail.com'}
+              target={'_blank'}
+              className={'md:flex md:items-center gap-2'}
+            >
+              <EnvelopeIcon className={'size-10 text-neutral-700'} />
+              <p className={'not-md:hidden'}>gdsc.yonsei.univ@gamil.com</p>
+            </Link>
+            <Link
+              href={'https://www.linkedin.com/company/gdsc-yonsei/'}
+              target={'_blank'}
+              className={'md:flex md:items-center gap-2 pl-1'}
+            >
+              <LinkedIn className={'size-9'} />
+              <p className={'not-md:hidden'}>go to LinkedIn</p>
+            </Link>
+            <Link
+              href={'https://www.instagram.com/gdg.yonseiuniv/'}
+              target={'_blank'}
+              className={'md:flex md:items-center gap-2'}
+            >
+              <Instagram className={'size-10'} />
+              <p className={'not-md:hidden'}>@gdg.yonseiuniv</p>
+            </Link>
+          </div>
+          <p
+            className={
+              'text-sm text-neutral-700 mx-auto text-center md:text-start md:w-full'
+            }
+          >
+            Copyright ⓒ 2025. GDG on Campus Yonsei <br />
+            All rights reserved.
+          </p>
+        </div>
       </div>
-      <p className={'text-sm text-neutral-700 mx-auto text-center'}>
-        Copyright ⓒ 2025. GDG on Campus Yonsei <br />
-        All rights reserved.
-      </p>
     </div>
   )
 }
