6afa9bafc8b39468d4c3a397a658945f24c6309c
jhyunwoo <jhyunwoo0228@gmail.com>
2025-03-05 16:59:04 +0900

    fix header mobile version style error

--- app/components/header/desktop-navigation-list.tsx
+++ app/components/header/desktop-navigation-list.tsx
@@ -2,7 +2,11 @@ import Link from 'next/link'
 
 export default function DesktopNavigationList() {
   return (
-    <div className={'flex items-center gap-2 *:p-1 *:px-4 *:hover:underline'}>
+    <div
+      className={
+        'flex items-center gap-2 *:p-1 *:px-4 *:hover:underline not-md:hidden'
+      }
+    >
       <Link href={'/members'}>Members</Link>
       <Link href={'/projects'}>Projects</Link>
       <Link href={'/sessions'}>Sessions</Link>
