90ea84a3fd135575ee2617c7bc6ddddc8696751c
jhyunwoo <jhyunwoo0228@gmail.com>
2025-06-19 14:38:07 +0900

    update images and logo

--- app/components/svg/gdgoc-yonsei-logo.tsx
+++ app/components/svg/gdgoc-yonsei-logo.tsx
@@ -9,10 +9,10 @@ export default function GDGoCYonseiLogo({ className }: { className?: string }) {
   return (
     <div className={`flex items-center gap-2 ${className}`}>
       <GDGLogo className={'w-16 md:w-24'} svgKey={'GDGoCYonseiLogo'} />
-      <p className={'text-xs md:text-base'}>
-        Google Developer Group <br />
-        Yonsei University
-      </p>
+      <div className={'flex flex-col'}>
+        <p className={'text-lg'}>Google Developer Group</p>
+        <p className={'text-blue-400'}>Yonsei University</p>
+      </div>
     </div>
   )
 }
