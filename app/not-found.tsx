27d49cd01b3aeaa22abe3bb55b7c19aa33b69168
jhyunwoo <jhyunwoo0228@gmail.com>
2025-08-23 19:31:47 +0900

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import GDGoCYonseiLogo from '@/app/components/svg/gdgoc-yonsei-logo'
+import './globals.css'
+
+/**
+ * 404 Not Found Page
+ * @constructor
+ */
+export default function NotFound() {
+  return (
+    <html>
+      <body>
+        <div className={'flex h-screen w-full items-center justify-center p-4'}>
+          <div className={'flex flex-col gap-4'}>
+            <GDGoCYonseiLogo />
+            <h1 className={'text-5xl font-bold md:text-6xl'}>404 Not Found</h1>
+            <Link href={'/'} className={'text-2xl font-semibold underline'}>
+              Back to Home
+            </Link>
+          </div>
+        </div>
+      </body>
+    </html>
+  )
+}
