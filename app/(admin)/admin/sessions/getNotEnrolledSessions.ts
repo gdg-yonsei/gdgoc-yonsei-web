0ceaef394436a6f8409d4cd399467b41de7eb08a
jhyunwoo <jhyunwoo0228@gmail.com>
2025-09-06 16:16:54 +0900

    add cache on session page

---
+++ app/(admin)/admin/sessions/getNotEnrolledSessions.ts
@@ -0,0 +1,56 @@
+import db from '@/db'
+import { userToSession } from '@/db/schema/user-to-session'
+import { and, asc, eq, gt, isNull, sql } from 'drizzle-orm'
+import { sessions } from '@/db/schema/sessions'
+import { parts } from '@/db/schema/parts'
+import cacheTag from '@/lib/server/cacheTag'
+
+export default async function getNotEnrolledSessions(userId: string) {
+  'use cache'
+  cacheTag('sessions')
+  const participantsSub = db
+    .select({
+      sessionId: userToSession.sessionId,
+      participantCount: sql<number>`COUNT(${userToSession.userId})`.as(
+        'participantCount'
+      ),
+    })
+    .from(userToSession)
+    .groupBy(userToSession.sessionId)
+    .as('participants')
+
+  const now = new Date()
+
+  return db
+    .select({
+      id: sessions.id,
+      name: sessions.nameKo,
+      startAt: sessions.startAt,
+      endAt: sessions.endAt,
+      location: sessions.locationKo,
+      maxCapacity: sessions.maxCapacity,
+      partId: sessions.partId,
+      part: parts.name,
+      mainImage: sessions.mainImage,
+      images: sessions.images,
+      participantCount: participantsSub.participantCount,
+    })
+    .from(sessions)
+    .innerJoin(parts, eq(sessions.partId, parts.id))
+    .leftJoin(participantsSub, eq(sessions.id, participantsSub.sessionId))
+    .leftJoin(
+      userToSession,
+      and(
+        eq(userToSession.sessionId, sessions.id),
+        eq(userToSession.userId, userId)
+      )
+    )
+    .where(
+      and(
+        eq(sessions.internalOpen, true),
+        gt(sessions.startAt, now),
+        isNull(userToSession.userId)
+      )
+    )
+    .orderBy(asc(sessions.startAt))
+}
