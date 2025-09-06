66a676cd21d025db5c78cc58b9309bf6f76ec067
jhyunwoo <jhyunwoo0228@gmail.com>
2025-09-06 22:59:55 +0900

    try to fix session order

--- lib/server/fetcher/admin/get-sessions.ts
+++ lib/server/fetcher/admin/get-sessions.ts
@@ -2,7 +2,6 @@ import { sessions } from '@/db/schema/sessions'
 import 'server-only'
 import db from '@/db'
 import { desc } from 'drizzle-orm'
-import { generations } from '@/db/schema/generations'
 import { cacheTag } from 'next/dist/server/use-cache/cache-tag'
 
 export const preload = () => {
@@ -24,6 +23,5 @@ export async function getSessions() {
         },
       },
     },
-    orderBy: desc(generations.id),
   })
 }
