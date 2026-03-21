7aac7cfb0ab98a9e6a8bf48140da116ae65789c2
jhyunwoo <jhyunwoo0228@gmail.com>
2026-03-21 18:46:13 +0900

    feat: Add `executedAt` and `errorMessage` fields to the `bookingRequests` schema.

--- db/schema/booking-requests.ts
+++ db/schema/booking-requests.ts
@@ -34,6 +34,8 @@ export const bookingRequests = pgTable('web_booking_requests', {
     .references(() => users.id, { onDelete: 'no action', onUpdate: 'cascade' }),
   createdAt: timestamp('createdAt').defaultNow().notNull(),
   updatedAt: timestamp('updatedAt').defaultNow().notNull(),
+  executedAt: timestamp('executedAt'),
+  errorMessage: text('errorMessage'),
 })
 
 export const bookingRequestsRelations = relations(
